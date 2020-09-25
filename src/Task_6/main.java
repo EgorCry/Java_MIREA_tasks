50d60af48e89bba4d828f8f9e041817785281147
EgorCry <abulapin@mail.ru>
2020-09-25 14:29:32 +0300

    update

--- src/Task_6/main.java
+++ src/Task_6/main.java
@@ -12,40 +12,58 @@ public class main {
                 massive[i][j] = scanner.nextInt();
             }
         }
-        int max = 0;
-        int k=0;
-        int max_x = 0, max_y = 0;
-        for (int i=0; i<N; i++){
-            for(int j=0;j<N;j++){
-                if(k==1){
-                    j--;
-                    k=0;
+        for (int i=0;i<N;i++){
+            for (int j=0; j<N;j++){
+                if(i>0&&j>0){
+                    massive[i][j]+=Math.max(massive[i-1][j],massive[i][j-1]);
                 }
-                max_x = massive[i][j];
-                max_y = massive[i][j];
-                max += massive[i][j];
-                for(int y=i;y<N;y++){
-                    for(int x=j;x<j+1;x++){
-                        if(massive[x][y]>max_y){
-                            max_y = massive[x][y];
-                        }
+                else {
+                    if (i>0){
+                        massive[i][j]+=massive[i-1][j];
+                    }
+                    else if(j>0){
+                        massive [i][j]+=massive[i][j-1];
                     }
                 }
-                for(int y=i;y<i+1;y++){
-                    for(int x=j;x<N;x++){
-                        if(massive[x][y]>max_x){
-                            max_x = massive[x][y];
-                        }
-                    }
-                }
-                System.out.println(i + " " + j);
-                System.out.println(max_x + " " + max_y);
-                if(max_y>=max_x){
-                    k=1;
-                    i+=1;
-                }
+
             }
         }
-        System.out.println(max);
+        System.out.print(massive[N-1][N-1]);
+
+//        int max = 0;
+//        int k=0;
+//        int max_x = 0, max_y = 0;
+//        for (int i=0; i<N; i++){
+//            for(int j=0;j<N;j++){
+//                if(k==1){
+//                    j--;
+//                    k=0;
+//                }
+//                max_x = massive[i][j];
+//                max_y = massive[i][j];
+//                max += massive[i][j];
+//                for(int y=i;y<N;y++){
+//                    for(int x=j;x<j+1;x++){
+//                        if(massive[x][y]>max_y){
+//                            max_y = massive[x][y];
+//                        }
+//                    }
+//                }
+//                for(int y=i;y<i+1;y++){
+//                    for(int x=j;x<N;x++){
+//                        if(massive[x][y]>max_x){
+//                            max_x = massive[x][y];
+//                        }
+//                    }
+//                }
+//                System.out.println(i + " " + j);
+//                System.out.println(max_x + " " + max_y);
+//                if(max_y>=max_x){
+//                    k=1;
+//                    i+=1;
+//                }
+//            }
+//        }
+//        System.out.println(max);
     }
 }
