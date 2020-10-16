b6a8ed88711fb0dd0ee3a75d288a68611ac5b575
EgorCry <abulapin@mail.ru>
2020-10-16 17:34:50 +0300

    update

--- src/Task_12/Main.java
+++ src/Task_12/Main.java
@@ -1,7 +1,13 @@
 package Task_12;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-
+        Colours obj = new Colours();
+        Scanner scanner = new Scanner(System.in);
+        String line = scanner.nextLine();
+        String colour = scanner.nextLine();
+        obj.print(line, colour);
     }
 }
