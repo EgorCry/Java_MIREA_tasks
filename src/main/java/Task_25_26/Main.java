4b201c19db6544c424baba91dd795133464e1440
EgorCry <abulapin@mail.ru>
2020-12-05 22:30:19 +0300

    update

--- src/main/java/Task_25_26/Main.java
+++ src/main/java/Task_25_26/Main.java
@@ -2,17 +2,20 @@ package Task_25_26;
 
 public class Main {
     public static void main(String[] args) {
-        HashMap<String, Integer> hashMap = new HashMap<>();
-        hashMap.add("k1", 1);
-        hashMap.add("k9", 9);
-        hashMap.add("k5", 5);
-        hashMap.add("k3", 3);
-        hashMap.add("k4", 4);
-        hashMap.add("k2", 2);
-        System.out.println("Get key 'k1': value=" + hashMap.get("k1"));
-        System.out.println("Get key 'k5': value=" + hashMap.get("k5"));
-        System.out.println("Remove key 'k3': value=" + hashMap.remove("k3"));;
-        for (Integer V : hashMap) {
+        HashMap<Integer, String> hashMap = new HashMap<>();
+        hashMap.add(1, "I");
+        hashMap.add(2, "am");
+        hashMap.add(3, "Batman");
+        hashMap.add(4, "I");
+        hashMap.add(5, "am");
+        hashMap.add(6, "vengeance");
+        System.out.println("Get key 1: value=" + hashMap.get(1));
+        System.out.println("Get key 2: value=" + hashMap.get(2));
+        System.out.println("Get key 3: value=" + hashMap.get(3));
+        System.out.println("Remove key 6: value=" + hashMap.remove(6));
+        hashMap.add(6, "psychopath");
+        System.out.println("Get key 6: value=" + hashMap.get(6));
+        for (String V : hashMap) {
             System.out.println(V);
         }
     }
